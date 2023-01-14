89af65e686398e1b65d597e7c87b0c1d2622e147
silas-inegbe <kingssilas18@gmail.co>
2023-01-14 09:19:43 -0800

    ggg

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,6 @@
 
 
 def print_last_digit(number):
-    last_digit = (number % 10) if number >= 0 else ((number * -1)  % 10)
+    last_digit = (number % 10) if number >= 0 else ((number * -1) % 10)
     print(last_digit, end='')
     return (last_digit)
